f8d83133afe8a1fabb2f2e67fc56ed3fda71b870
Abdallah1Habib <abdullah.muhammad.200185@btu.edu.eg>
2023-03-02 22:02:51 +0200

    making the text field accept variables to use in different cases

--- project1/lib/widgets/mytextfield.dart
+++ project1/lib/widgets/mytextfield.dart
@@ -1,15 +1,22 @@
 import 'package:flutter/material.dart';
 
 class CustomTextField extends StatelessWidget {
-  const CustomTextField({super.key});
+  const CustomTextField(
+      {super.key,
+      required this.pass,
+      required this.text,
+      required this.inputtype});
+  final bool pass;
+  final String text;
+  final TextInputType inputtype;
 
   @override
   Widget build(BuildContext context) {
     return TextField(
-      keyboardType: TextInputType.emailAddress,
-      obscureText: false,
+      keyboardType: inputtype,
+      obscureText: pass,
       decoration: InputDecoration(
-        hintText: "Enter your email : ",
+        hintText: text,
         enabledBorder:
             OutlineInputBorder(borderSide: Divider.createBorderSide(context)),
         focusedBorder: const OutlineInputBorder(

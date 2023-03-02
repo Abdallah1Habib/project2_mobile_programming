791313e72e65641ccb5bc3045f12918bb99e692c
Abdallah1Habib <abdullah.muhammad.200185@btu.edu.eg>
2023-03-02 19:58:21 +0200

    removing the debug banner from the app

--- project1/lib/main.dart
+++ project1/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:project1/screens/login.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,7 +12,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: mainbody();
+      debugShowCheckedModeBanner: false,
+      home: loginpage();
       );
   }
 }

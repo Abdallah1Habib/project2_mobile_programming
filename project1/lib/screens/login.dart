4feb0f9b4302a58870c5a80c22a1655b827eb5fc
Abdallah1Habib <abdullah.muhammad.200185@btu.edu.eg>
2023-03-06 16:47:58 +0200

    wraping the column in expanded to avoid collision with keyboard when typing

--- project1/lib/screens/login.dart
+++ project1/lib/screens/login.dart
@@ -16,52 +16,53 @@ class loginpage extends StatelessWidget {
         backgroundColor: Colors.grey[150],
         body: Padding(
           padding: const EdgeInsets.all(30.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              SizedBox(
-                height: 64,
-              ),
-              CustomTextField(
-                  pass: false,
-                  text: "Enter Email : ",
-                  inputtype: TextInputType.emailAddress),
-              SizedBox(
-                height: 30,
-              ),
-              CustomTextField(
-                  pass: true,
-                  text: "Enter Password : ",
-                  inputtype: TextInputType.text),
-              SizedBox(
-                height: 30,
-              ),
-              ElevatedButton(
-                onPressed: () {},
-                child: Text("Login", style: TextStyle(fontSize: 20)),
-                style: ButtonStyle(
-                    backgroundColor: MaterialStateProperty.all(mainbuttoncolor),
-                    padding: MaterialStateProperty.all(EdgeInsets.all(12)),
-                    shape: MaterialStateProperty.all(RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(8)))),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text("Don't have an account?",
-                      style: TextStyle(color: Colors.black, fontSize: 20)),
-                  TextButton(
+          child: Expanded(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                CustomTextField(
+                    pass: false,
+                    text: "Enter Email : ",
+                    inputtype: TextInputType.emailAddress),
+                SizedBox(
+                  height: 30,
+                ),
+                CustomTextField(
+                    pass: true,
+                    text: "Enter Password : ",
+                    inputtype: TextInputType.text),
+                SizedBox(
+                  height: 30,
+                ),
+                ElevatedButton(
+                  onPressed: () {},
+                  child: Text("Login", style: TextStyle(fontSize: 20)),
+                  style: ButtonStyle(
+                      backgroundColor:
+                          MaterialStateProperty.all(mainbuttoncolor),
+                      padding: MaterialStateProperty.all(EdgeInsets.all(12)),
+                      shape: MaterialStateProperty.all(RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8)))),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text("Don't have an account?",
+                        style: TextStyle(color: Colors.black, fontSize: 20)),
+                    TextButton(
                       onPressed: () {},
                       child: Text(
                         "Sign Up",
                         style: TextStyle(color: Colors.black, fontSize: 20),
-                      )),
-                ],
-              ),
-            ],
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),

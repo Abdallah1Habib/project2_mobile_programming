1f701b5422904108578070358a41cdca827fad2b
Abdallah1Habib <abdullah.muhammad.200185@btu.edu.eg>
2023-03-06 16:36:25 +0200

    adding a button for new users to sign up

--- project1/lib/screens/login.dart
+++ project1/lib/screens/login.dart
@@ -12,36 +12,53 @@ class loginpage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Padding(
-      padding: const EdgeInsets.all(30.0),
-      child: Column(children: [
-        SizedBox(
-          height: 64,
+      body: Padding(
+        padding: const EdgeInsets.all(30.0),
+        child: Column(
+          children: [
+            SizedBox(
+              height: 64,
+            ),
+            CustomTextField(
+                pass: false,
+                text: "Enter Email : ",
+                inputtype: TextInputType.emailAddress),
+            SizedBox(
+              height: 30,
+            ),
+            CustomTextField(
+                pass: true,
+                text: "Enter Password : ",
+                inputtype: TextInputType.text),
+            SizedBox(
+              height: 30,
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: Text("Login", style: TextStyle(fontSize: 20)),
+              style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(mainbuttoncolor),
+                  padding: MaterialStateProperty.all(EdgeInsets.all(12)),
+                  shape: MaterialStateProperty.all(RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8)))),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Row(
+              children: [
+                Text("Don't have an account"),
+                TextButton(
+                    onPressed: () {},
+                    child: Text(
+                      "Sign Up",
+                      style: TextStyle(color: Colors.black),
+                    )),
+              ],
+            ),
+          ],
         ),
-        CustomTextField(
-            pass: false,
-            text: "Enter Email : ",
-            inputtype: TextInputType.emailAddress),
-        SizedBox(
-          height: 30,
-        ),
-        CustomTextField(
-            pass: true,
-            text: "Enter Password : ",
-            inputtype: TextInputType.text),
-        SizedBox(
-          height: 30,
-        ),
-        ElevatedButton(
-          onPressed: () {},
-          child: Text("Login", style: TextStyle(fontSize: 20)),
-          style: ButtonStyle(
-              backgroundColor: MaterialStateProperty.all(mainbuttoncolor),
-              padding: MaterialStateProperty.all(EdgeInsets.all(12)),
-              shape: MaterialStateProperty.all(RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(8)))),
-        )
-      ]),
-    ));
+      ),
+    );
   }
 }

4c9baaeed416e95f6b18e93987c63dbaa422bb99
Abdallah1Habib <abdullah.muhammad.200185@btu.edu.eg>
2023-03-06 16:45:32 +0200

    wraping the scaffold in a safe area to give space for the phone notifications bar

--- project1/lib/screens/login.dart
+++ project1/lib/screens/login.dart
@@ -11,56 +11,58 @@ class loginpage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.grey[150],
-      body: Padding(
-        padding: const EdgeInsets.all(30.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            SizedBox(
-              height: 64,
-            ),
-            CustomTextField(
-                pass: false,
-                text: "Enter Email : ",
-                inputtype: TextInputType.emailAddress),
-            SizedBox(
-              height: 30,
-            ),
-            CustomTextField(
-                pass: true,
-                text: "Enter Password : ",
-                inputtype: TextInputType.text),
-            SizedBox(
-              height: 30,
-            ),
-            ElevatedButton(
-              onPressed: () {},
-              child: Text("Login", style: TextStyle(fontSize: 20)),
-              style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.all(mainbuttoncolor),
-                  padding: MaterialStateProperty.all(EdgeInsets.all(12)),
-                  shape: MaterialStateProperty.all(RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(8)))),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text("Don't have an account?",
-                    style: TextStyle(color: Colors.black, fontSize: 20)),
-                TextButton(
-                    onPressed: () {},
-                    child: Text(
-                      "Sign Up",
-                      style: TextStyle(color: Colors.black, fontSize: 20),
-                    )),
-              ],
-            ),
-          ],
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Colors.grey[150],
+        body: Padding(
+          padding: const EdgeInsets.all(30.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(
+                height: 64,
+              ),
+              CustomTextField(
+                  pass: false,
+                  text: "Enter Email : ",
+                  inputtype: TextInputType.emailAddress),
+              SizedBox(
+                height: 30,
+              ),
+              CustomTextField(
+                  pass: true,
+                  text: "Enter Password : ",
+                  inputtype: TextInputType.text),
+              SizedBox(
+                height: 30,
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                child: Text("Login", style: TextStyle(fontSize: 20)),
+                style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(mainbuttoncolor),
+                    padding: MaterialStateProperty.all(EdgeInsets.all(12)),
+                    shape: MaterialStateProperty.all(RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8)))),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text("Don't have an account?",
+                      style: TextStyle(color: Colors.black, fontSize: 20)),
+                  TextButton(
+                      onPressed: () {},
+                      child: Text(
+                        "Sign Up",
+                        style: TextStyle(color: Colors.black, fontSize: 20),
+                      )),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );

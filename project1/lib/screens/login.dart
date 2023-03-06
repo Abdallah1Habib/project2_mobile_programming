b6ebce4de5d4c1332969d5fdf19990d583f42a1e
Abdallah1Habib <abdullah.muhammad.200185@btu.edu.eg>
2023-03-06 16:43:36 +0200

    changing the background color to enhance visuals

--- project1/lib/screens/login.dart
+++ project1/lib/screens/login.dart
@@ -12,6 +12,7 @@ class loginpage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[150],
       body: Padding(
         padding: const EdgeInsets.all(30.0),
         child: Column(
@@ -44,7 +45,7 @@ class loginpage extends StatelessWidget {
                       borderRadius: BorderRadius.circular(8)))),
             ),
             SizedBox(
-              height: 30,
+              height: 20,
             ),
             Row(
               mainAxisAlignment: MainAxisAlignment.center,

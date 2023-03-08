726c59896e1744a06cef6c248847ded433e650ea
Abdallah1Habib <abdullah.muhammad.200185@btu.edu.eg>
2023-03-08 20:53:49 +0200

    adding the images paths in the list

--- project1/lib/screens/home.dart
+++ project1/lib/screens/home.dart
@@ -11,9 +11,11 @@ class item {
 
 class homepage extends StatelessWidget {
   List itms = [
-    item(imgpath: "", price: 12.99),
-    item(imgpath: "", price: 12.99),
-    item(imgpath: "", price: 12.99),
+    item(imgpath: "lec2_project/project1/assets/images/1.jpg", price: 12.99),
+    item(imgpath: "lec2_project/project1/assets/images/2.jpg", price: 15.99),
+    item(imgpath: "lec2_project/project1/assets/images/3.jpg", price: 13.99),
+    item(imgpath: "lec2_project/project1/assets/images/4.jpg", price: 21.99),
+    item(imgpath: "lec2_project/project1/assets/images/5.jpg", price: 9.99),
   ];
 
   @override

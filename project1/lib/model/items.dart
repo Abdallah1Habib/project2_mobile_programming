c975245c6e12fe1a2d4bc8f104806aa802364124
Abdallah1Habib <abdullah.muhammad.200185@btu.edu.eg>
2023-03-08 21:13:51 +0200

    fixing error in items path

--- project1/lib/model/items.dart
+++ project1/lib/model/items.dart
@@ -5,9 +5,9 @@ class item {
 }
 
 List<item> items = [
-  item(imgpath: "lec2_project/project1/assets/images/1.jpg", price: 12.99),
-  item(imgpath: "lec2_project/project1/assets/images/2.jpg", price: 15.99),
-  item(imgpath: "lec2_project/project1/assets/images/3.jpg", price: 13.99),
-  item(imgpath: "lec2_project/project1/assets/images/4.jpg", price: 21.99),
-  item(imgpath: "lec2_project/project1/assets/images/5.jpg", price: 9.99),
+  item(imgpath: "assets/images/1.jpg", price: 12.99),
+  item(imgpath: "assets/images/2.jpg", price: 15.99),
+  item(imgpath: "assets/images/3.jpg", price: 13.99),
+  item(imgpath: "assets/images/4.jpg", price: 21.99),
+  item(imgpath: "assets/images/5.jpg", price: 9.99),
 ];

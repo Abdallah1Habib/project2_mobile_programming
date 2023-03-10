0843695b80d51751ce09c627fabd2f665c795b77
Abdallah1Habib <abdullah.muhammad.200185@btu.edu.eg>
2023-03-10 10:29:12 +0200

    built a function that adds items to the empty list

--- project1/lib/provider/cart.dart
+++ project1/lib/provider/cart.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:project1/model/items.dart';
 
 class Cart with ChangeNotifier {
   List selectprod = [];
+
+  additem(item product){
+    selectprod.add(product);
+  }
 }

23a8eb31a5c1e4dd0bc5ca26d8927bb40b07c5cc
Abdallah1Habib <abdullah.muhammad.200185@btu.edu.eg>
2023-03-10 11:05:28 +0200

    building the app bar file with the consumer

---
+++ project1/lib/widgets/appbar.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class CustApp extends StatelessWidget {
+  const CustApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<Cart>(builder: ((context, newx, child) {
+      Row(
+        children: [
+          Stack(
+            children: [
+              Positioned(
+                bottom: 24,
+                child: Container(
+                  padding: const EdgeInsets.all(5),
+                  decoration: const BoxDecoration(
+                      color: Color.fromARGB(211, 164, 255, 193),
+                      shape: BoxShape.circle),
+                  child: Text(
+                    "${newx.selectprod.length}",
+                    style: TextStyle(fontSize: 16, color: Colors.black),
+                  ),
+                ),
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(Icons.add_shopping_cart),
+              ),
+            ],
+          ),
+          Padding(
+            padding: EdgeInsets.only(right: 12),
+            child: Text(
+              "\$ ${newx.price}",
+              style: TextStyle(color: Colors.black),
+            ),
+          ),
+        ],
+      );
+    }));
+  }
+}

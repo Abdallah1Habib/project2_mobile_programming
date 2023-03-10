a8d339fc7b5565d91cbedab5be8f3806a9d8a87f
Abdallah1Habib <abdullah.muhammad.200185@btu.edu.eg>
2023-03-10 10:42:03 +0200

    making a price variable to store items prices

--- project1/lib/provider/cart.dart
+++ project1/lib/provider/cart.dart
@@ -4,6 +4,8 @@ import 'package:project1/model/items.dart';
 class Cart with ChangeNotifier {
   List selectprod = [];
 
+  double price = 0;
+
   additem(item product) {
     selectprod.add(product);
     notifyListeners();

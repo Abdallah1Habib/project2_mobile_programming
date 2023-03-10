a1372d0f91397e58a6445b3bf80d0fee4522befc
Abdallah1Habib <abdullah.muhammad.200185@btu.edu.eg>
2023-03-10 11:12:01 +0200

    building the app bar of the checkout

--- project1/lib/screens/checkout.dart
+++ project1/lib/screens/checkout.dart
@@ -6,6 +6,10 @@ class Checkout extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Checkout"),
+      ),
+    );
   }
 }

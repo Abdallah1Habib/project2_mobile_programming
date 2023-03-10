20b61bafb153e8fcdd463ef7a5bcfa610e1b9797
Abdallah1Habib <abdullah.muhammad.200185@btu.edu.eg>
2023-03-10 11:09:35 +0200

    calling the custom app bar in the details screen

--- project1/lib/screens/details.dart
+++ project1/lib/screens/details.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 import 'package:project1/model/items.dart';
+import 'package:project1/widgets/appbar.dart';
 
 import '../constants.dart';
 
@@ -28,39 +29,8 @@ class _DetailsState extends State<Details> {
           })
         ),
         actions: [
-          Row(
-            children: [
-              Stack(
-                children: [
-                  Positioned(
-                    bottom: 24,
-                    child: Container(
-                      padding: const EdgeInsets.all(5),
-                      decoration: const BoxDecoration(
-                          color: Color.fromARGB(211, 164, 255, 193),
-                          shape: BoxShape.circle),
-                      child: const Text(
-                        "8",
-                        style: TextStyle(fontSize: 16, color: Colors.black),
-                      ),
-                    ),
-                  ),
-                  IconButton(
-                    onPressed: () {},
-                    icon: const Icon(Icons.add_shopping_cart),
-                  ),
-                ],
-              ),
-              const Padding(
-                padding: EdgeInsets.only(right: 12),
-                child: Text(
-                  "\$ 15",
-                  style: TextStyle(color: Colors.black),
-                ),
-              ),
-            ],
-          ),
-        ],
+          CustApp(),
+          ],
       ),
       body: SingleChildScrollView(
         child: Column(
